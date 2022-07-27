11989fa0a1c52b9dfb3400cd38ed9f4852d91a61
Erick Scur <erickscur@gmail.com>
2022-07-27 20:56:24 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,13 +2,19 @@ import { DbAddAccount } from '../../data/usecases/add-account/db-add-account';
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter';
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account';
 import { SignUpController } from '../../presentation/controllers/signup/signup';
+import { Controller } from '../../presentation/protocols';
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter';
+import { LogControllerDecorator } from '../decorators/log';
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12;
   const bcryptAdapater = new BcryptAdapter(salt);
   const emailValidatorAdapter = new EmailValidatorAdapter();
   const addAccountRepository = new AccountMongoRepository();
   const dbAddAccount = new DbAddAccount(bcryptAdapater, addAccountRepository);
-  return new SignUpController(emailValidatorAdapter, dbAddAccount);
+  const signupController = new SignUpController(
+    emailValidatorAdapter,
+    dbAddAccount,
+  );
+  return new LogControllerDecorator(signupController);
 };
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -48,7 +48,6 @@ export class SignUpController implements Controller {
 
       return ok(account);
     } catch (error) {
-      console.error(error);
       return serverError();
     }
   }
