488695f292afc8dfa551d3d910a1e214284e3e5b
Erick Scur <erickscur@gmail.com>
2022-08-09 21:17:43 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -0,0 +1,44 @@
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository';
+import { AccountModel } from '../add-account/db-add-account-protocols';
+import { DbAuthentication } from './db-authentication';
+
+interface sutTypes {
+  sut: DbAuthentication;
+  loadAccountByEmailRepositorystub: LoadAccountByEmailRepository;
+}
+
+const makeSut = (): sutTypes => {
+  class LoadAccountByEmailRepositoryStub
+    implements LoadAccountByEmailRepository
+  {
+    async load(email: string): Promise<AccountModel> {
+      const account: AccountModel = {
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      };
+      return new Promise((resolve) => resolve(account));
+    }
+  }
+
+  const loadAccountByEmailRepositorystub =
+    new LoadAccountByEmailRepositoryStub();
+  const sut = new DbAuthentication(loadAccountByEmailRepositorystub);
+  return {
+    sut,
+    loadAccountByEmailRepositorystub,
+  };
+};
+
+describe('DbAuthentication UseCase', () => {
+  test('Should call LoadAccountByEmailRepository with correct email', async () => {
+    const { sut, loadAccountByEmailRepositorystub } = makeSut();
+    const loadSpy = jest.spyOn(loadAccountByEmailRepositorystub, 'load');
+    await sut.auth({
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+    expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com');
+  });
+});
