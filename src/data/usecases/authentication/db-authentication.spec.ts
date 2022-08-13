182dfc517420d9d76816bd5b8f33b2e7575a2650
Erick Scur <erickscur@gmail.com>
2022-08-13 13:27:26 -0300

    test: ensure DbAuthentication returns null if HashComparer returns false

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -114,4 +114,16 @@ describe('DbAuthentication UseCase', () => {
     });
     await expect(promise).rejects.toThrow();
   });
+
+  test('Should return null if HashComparer returns false', async () => {
+    const { sut, hashComparerStub } = makeSut();
+    jest
+      .spyOn(hashComparerStub, 'compare')
+      .mockReturnValueOnce(new Promise((resolve) => resolve(false)));
+    const accessToken = await sut.auth({
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+    expect(accessToken).toBe(null);
+  });
 });
