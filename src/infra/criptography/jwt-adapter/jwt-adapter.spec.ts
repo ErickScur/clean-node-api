a2f824bfead477b0456cbacfe2cc9fbe80b2ea19
Erick Scur <erickscur@gmail.com>
2022-08-18 08:03:55 -0300

    refactor: move sut creation to a factory function

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,9 +1,12 @@
 import jwt from 'jsonwebtoken';
 import { JwtAdapter } from './jwt-adapter';
 
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter('secret');
+};
 describe('Jwt Adapter', () => {
   test('Should call sign with correct values', async () => {
-    const sut = new JwtAdapter('secret');
+    const sut = makeSut();
     const signSpy = jest.spyOn(jwt, 'sign');
     await sut.encrypt('any_value');
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret');
