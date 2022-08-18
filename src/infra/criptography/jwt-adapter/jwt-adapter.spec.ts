cd5f94ec844577598280bfd1c89239609e437b93
Erick Scur <erickscur@gmail.com>
2022-08-18 08:09:04 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -23,4 +23,14 @@ describe('Jwt Adapter', () => {
     const accessToken = await sut.encrypt('any_value');
     expect(accessToken).toBe('any_token');
   });
+
+  test('Should throw if sign throws', async () => {
+    const sut = makeSut();
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const promise = sut.encrypt('any_value');
+
+    await expect(promise).rejects.toThrow();
+  });
 });
