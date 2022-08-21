171371c03003827a1ff8a7c93725ebc8c7465024
Erick Scur <erickscur@gmail.com>
2022-08-21 14:26:43 -0300

    feat: MongoHelper parseToObjectId

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { Collection, MongoClient } from 'mongodb';
+import { Collection, MongoClient, ObjectId } from 'mongodb';
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -19,5 +19,9 @@ export const MongoHelper = {
       await this.connect(this.uri);
     }
     return this.client.db().collection(name);
+  },
+
+  parseToObjectId(value: string): ObjectId {
+    return new ObjectId(value);
   },
 };
