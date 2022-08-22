0d0e0b5292c5ab23472e88834e926ff42481470e
Erick Scur <erickscur@gmail.com>
2022-08-21 21:17:00 -0300

    refactor: change infra layer folders and files names

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,7 +1,7 @@
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account';
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter';
-import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account';
-import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log';
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository';
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository';
 import { SignUpController } from '../../../presentation/controllers/signup/signup';
 import { Controller } from '../../../presentation/protocols';
 import { LogControllerDecorator } from '../../decorators/log';
