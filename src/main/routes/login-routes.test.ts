44e51bd0f7500f4b8926c0a2bda4ed3d94f494d8
Erick Scur <erickscur@gmail.com>
2022-08-23 21:18:10 -0300

    test: ensure login route returns 401 if login fails

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -52,5 +52,15 @@ describe('Login Routes', () => {
         })
         .expect(200);
     });
+
+    test('Should return 401 if login fails', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'valid_email@mail.mail',
+          password: '123',
+        })
+        .expect(401);
+    });
   });
 });
